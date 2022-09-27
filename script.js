2539adf0c3d0d906fcb5b8c6ce2d9a3cdb8b7b98
Ayush Kumar Saxena <ayushsaxena119@gmail.com>
2022-09-27 23:41:47 +0530

    fixed input validation

--- script.js
+++ script.js
@@ -73,27 +73,33 @@ function myfunc(action) {
       let header = document.getElementById("form233");
       header.innerText = plainText;
     } else if (check == "rfc") {
-  
-        var ciphertext = document.getElementById("form23").value;
-        var key = document.getElementById("in").value;
-        if(ciphertext.length < 1){ alert("please enter some ciphertext (letters only)"); return; }    
-        
-        if(key > Math.floor(2*(ciphertext.length-1))){ alert("please enter 1 - 22."); return; }      
-        pt = new Array(ciphertext.length);   k=0;
-        for(line=0; line<key-1; line++){
-           skip=2*(key-line-1);  j=0;
-            for(i=line; i<ciphertext.length;){
-                pt[i] = ciphertext.charAt(k++);
-                if((line==0) || (j%2 == 0)) i+=skip;
-               else i+=2*(key-1) - skip;  
-               j++;        
-            }
+      var ciphertext = document.getElementById("form23").value;
+      var key = document.getElementById("in").value;
+      if (ciphertext.length < 1) {
+        alert("please enter some ciphertext (letters only)");
+        return;
+      }
+
+      if (key > Math.floor(2 * (ciphertext.length - 1))) {
+        alert("please enter 1 - 22.");
+        return;
+      }
+      pt = new Array(ciphertext.length);
+      k = 0;
+      for (line = 0; line < key - 1; line++) {
+        skip = 2 * (key - line - 1);
+        j = 0;
+        for (i = line; i < ciphertext.length; ) {
+          pt[i] = ciphertext.charAt(k++);
+          if (line == 0 || j % 2 == 0) i += skip;
+          else i += 2 * (key - 1) - skip;
+          j++;
         }
-        for(i=line; i<ciphertext.length; i+=2*(key-1)) pt[i] = ciphertext.charAt(k++);
-        let header = document.getElementById("form233");
-        header.innerText = pt.join("");
-        
-  
+      }
+      for (i = line; i < ciphertext.length; i += 2 * (key - 1))
+        pt[i] = ciphertext.charAt(k++);
+      let header = document.getElementById("form233");
+      header.innerText = pt.join("");
     }
   }
 } // End of script.js
@@ -118,7 +124,8 @@ function invalidinput(action) {
     !form.test(document.getElementById("in").value)
   ) {
     let header = document.getElementById("form233");
-    header.innerText = "Invalid Input";
+    header.innerText =
+      "Invalid Input: Please enter letters only in input area and numbers only in key area";
   } else {
     myfunc(action);
   }
